9f99ca7561e77ecb16885e53494e64df0ff71ad9
LoZaico <lozaico0098@gmail.com>
2024-05-29 19:53:52 -0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { MyFirstComponent } from './components/MyFirstComponent';
-import { SecondComponent } from './components/SecondComponent';
+import { MyFristComponents } from './components/MyFristComponents';
+import { SecondComponents } from './components/SecondComponents';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hook useState</h2>
-        <div>
-          <hr />
-          <MyFirstComponent />
-          <hr />
-          <SecondComponent />
-        </div>
+        <h2>Hook UseState</h2>
+        <MyFristComponents/>
+        <hr />
+        <SecondComponents/>
       </header>
     </div>
   );
--- src/components/MyFirstComponent.jsx
+++ src/components/MyFirstComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
 
-export const MyFirstComponent = () => {
-
-  const [ counter, setCounter ] = useState(0);
+export const MyFristComponents = () => {
+    const [ counter, setCounter] = useState(0);
 
   return (
     <div>
-        <h3>El número del contador es {counter}</h3>
-        <button type="submit" className='button-large' onClick={() => setCounter(counter +1)}>Sumar Contador</button>
+        <h3>El número del contador es {counter} </h3>
+        <button type='submit' onClick={() => setCounter(counter +1)} className='button-large'>
+            Sumar contador
+        </button>
     </div>
   )
 }
